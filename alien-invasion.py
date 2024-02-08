f9a116fda995f8c5efffb48f6d55663dc0a2ed29
denimwenda <denimwendwa@gmail.com>
2024-02-08 19:52:47 +0300

    changes

--- alien-invasion.py
+++ alien-invasion.py
@@ -2,27 +2,48 @@ import sys
 
 import pygame
 
+from pygame.sprite import Group
 from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 def run_game():
-    #Initialize pygame, settings and screen object.
+    # Initialize pygame, settings and screen object.
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    #Setting the backgroun color.
+    # Make a ship
+    ship = Ship(ai_settings, screen)
+    # Make a group to store bullets
+    bullets= (Group)
+    ship = Ship(screen)
+
+    # Setting the backgroun color.
     bg_color = (230, 230, 230)
 
-    #Start the main loop for the game.
+    # Start the main loop for the game.
     while True:
-        #Watch for keyboard and mouse events.
+        # Watch for keyboard and mouse events.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        gf.check_events(ai_settings, screen,ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+
+        # Get rid of bullets that has dissappeared.
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove()
+        print(len(bullets))
+        gf.update_screen(ai_settings, screen, ship, bullets)
         
         #Redraw the screen during each pass through the loop.
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
         #Make the ost recent drawn screen visible.
         pygame.display.flip()
 
